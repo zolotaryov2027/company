3d44614107ae6896c41b77991498c752a5224392
zolotaryov27207 <anton.zolotaryov27@yandex.ru>
2026-03-06 14:05:58 +0300

    update api

--- src/app/api/fake.api/professions.api.js
+++ src/app/api/fake.api/professions.api.js
@@ -1,3 +1,12 @@
+export const professionsObject = {
+  doctor: { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" },
+  waiter: { _id: "67rdca3eeb7f6fgeed471820", name: "Официант" },
+  physics: { _id: "67rdca3eeb7f6fgeed471814", name: "Физик" },
+  engineer: { _id: "67rdca3eeb7f6fgeed471822", name: "Инженер" },
+  actor: { _id: "67rdca3eeb7f6fgeed471824", name: "Актер" },
+  cook: { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" },
+};
+
 export const professions = {
   doctor: { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" },
   waiter: { _id: "67rdca3eeb7f6fgeed471820", name: "Официант" },
