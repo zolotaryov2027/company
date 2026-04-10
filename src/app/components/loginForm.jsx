73737f169e4a79eb051ca16655ed527dd71724f4
zolotaryov27207 <anton.zolotaryov27@yandex.ru>
2026-04-10 14:09:51 +0300

    add LoginForm

---
+++ src/app/components/loginForm.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import validator from "../utils/validator";
+import TextField from "./textField";
+
+const LoginForm = () => {
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+
+  const validatorConfig = {
+    email: {
+      isRequired: { message: "Электронная почта обязательна для заполнения" },
+      isEmail: { message: "Email введен некорректно" },
+    },
+    password: {
+      isRequired: { message: "Пароль обязательный для заполнения" },
+      isCapitalSymbol: {
+        message: "Пароль должен содержать хотя бы одну заглавную букву",
+      },
+      isContainDigit: {
+        message: "Пароль должен содержать хотя бы одно число",
+      },
+      min: {
+        message: "Пароль должен состоять минимум из 8 символов",
+        value: 8,
+      },
+    },
+  };
+
+  const validate = () => {
+    const errors = validator(data, validatorConfig);
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+  const isValid = Object.keys(errors).length === 0;
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    validate();
+  }, [data]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    console.log(Object.keys(errors).length === 0);
+    if (!isValid) return;
+    console.log(data);
+  };
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 p-4 shadow">
+          <h3 className="mb-4">Login</h3>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Email:"
+              name="email"
+              value={data.name}
+              onChange={handleChange}
+              error={errors.email}
+            />
+            <TextField
+              label="Password:"
+              type="password"
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+              error={errors.password}
+            />
+            <button
+              type="submit"
+              disabled={!isValid}
+              className="btn btn-primary w-100 mx-auto"
+            >
+              Отправить
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
